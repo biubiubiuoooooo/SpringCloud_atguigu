541e05b9fba28a02b7c31e1a6ba18c7f24de6e3a
15998 <15998808970@163.com>
2022-11-09 15:52:24 +0800

    启动类

---
+++ cloud-provider-payment8001/src/main/java/com/cao/springcloud/payment8001.java
@@ -0,0 +1,12 @@
+package com.cao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class payment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(payment8001.class, args);
+    }
+}
