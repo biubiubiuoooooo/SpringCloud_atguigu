a0644432a5215eed6f22bb178bea1a0a5a31860d
15998 <15998808970@163.com>
2022-11-09 17:27:25 +0800

    消费者模块

--- cloud-consumer-order80/src/main/java/com/cao/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/cao/springcloud/controller/OrderController.java
@@ -44,40 +44,4 @@ public class OrderController
     {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
-
-    @GetMapping("/consumer/payment/getForEntity/{id}")
-    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
-    {
-        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
-
-        if(entity.getStatusCode().is2xxSuccessful()){
-            return entity.getBody();
-        }else{
-            return new CommonResult<>(444,"操作失败");
-        }
-    }
-
-//    @GetMapping(value = "/consumer/payment/lb")
-//    public String getPaymentLB() {
-//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-//
-//        if(instances == null || instances.size() <= 0)
-//        {
-//            return null;
-//        }
-//
-//        ServiceInstance serviceInstance = loadBalancer.instances(instances);
-//        URI uri = serviceInstance.getUri();
-//
-//        return restTemplate.getForObject(uri+"/payment/lb",String.class);
-//
-//    }
-
-    // ====================> zipkin+sleuth
-    @GetMapping("/consumer/payment/zipkin")
-    public String paymentZipkin()
-    {
-        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
-        return result;
-    }
 }
