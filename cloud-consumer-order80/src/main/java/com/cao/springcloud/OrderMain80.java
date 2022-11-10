7330eca369779841d19c747c7ee89fc40e16a168
15998 <15998808970@163.com>
2022-11-10 14:49:06 +0800

    消费者模块

--- cloud-consumer-order80/src/main/java/com/cao/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/cao/springcloud/OrderMain80.java
@@ -3,12 +3,14 @@ package com.cao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @auther zzyy
  * @create 2020-02-18 17:20
  */
 @SpringBootApplication
+@EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80
 {
